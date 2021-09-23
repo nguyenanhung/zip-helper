3c8b58a00002cb560e8594dd1b02cc113ed66836
nguyenanhung <info@nguyenanhung.com>
2021-09-23 13:28:17 +0700

    Deplloy

--- test/test.php
+++ test/test.php
@@ -1,2 +1,17 @@
 <?php
+
+use nguyenanhung\Libraries\Zip\Zip;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$zip = new Zip();
+$zip->setFilename(__DIR__ . '/../tmp/test-file.zip');
+$zip->setDataList(
+    array(
+        __DIR__ . '/../docs'
+    )
+);
+
+$result = $zip->zip();
+
+d($result);
