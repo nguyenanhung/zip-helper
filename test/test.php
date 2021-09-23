d9907cdf5099f25f90cee4fa0bea53c102c07077
nguyenanhung <info@nguyenanhung.com>
2021-09-23 13:35:37 +0700

    Test Extract

--- test/test.php
+++ test/test.php
@@ -4,15 +4,22 @@ use nguyenanhung\Libraries\Zip\Zip;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$fileTest = __DIR__ . '/../tmp/test-file.zip';
+
 $zip = new Zip();
-$zip->setFilename(__DIR__ . '/../tmp/test-file.zip');
+$zip->setFilename($fileTest);
 $zip->setDataList(
     array(
         __DIR__ . '/../docs'
     )
 );
-
 $result = $zip->zip();
 
 d($result);
 d(directory_map(__DIR__ . '/../tmp'));
+
+$zip->setExtractFolder(__DIR__ . '/../tmp/testExtractFolder');
+$extract = $zip->unzip();
+d($extract);
+d(directory_map(__DIR__ . '/../tmp'));
+d(directory_map(__DIR__ . '/../tmp/testExtractFolder'));
